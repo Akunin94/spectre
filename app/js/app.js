e28764909583fd92fe86ebc215563ee68ff4e4b9
Davron Usmonov <akunin94@gmail.com>
2021-11-19 22:53:41 +0500

    1

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,12 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import $ from 'jquery';
 
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-
-})
+$(function(){
+	// BURGER START
+	$(document).on('click', '.spectre-header__burger', function(){
+		$('.spectre-header__popup').addClass('active');
+	});
+	$(document).on('click', '.spectre-header__popup-close', function(){
+		$('.spectre-header__popup').removeClass('active');
+	});
+	// BURGER END
+});
