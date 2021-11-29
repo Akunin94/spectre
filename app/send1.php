b667bcb88b80f9590159307c3bb90097776d81ac
Davron Usmonov <akunin94@gmail.com>
2021-11-29 22:31:51 +0500

    14

--- app/send1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    
-    mail('gromovs1989@gmail.com', 'Заявка с сайта: СПЕКТР', "Имя: {$name}, Телефон: {$phone}");
-
-    header("Location: /");
-    die();
